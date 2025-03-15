d41c4f0146460e21629af26b7098092f34bb88d3
Tvshows122 <126122240+Tvshows122@users.noreply.github.com>
2025-03-15 17:55:47 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Tvshows122/rocky-2025.git /TheMovieProviderBot
+  git clone https://github.com/Tvshows122/rocky-2025.git /TheMovieProviderBot1
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TheMovieProviderBot
+  git clone $UPSTREAM_REPO /TheMovieProviderBot1
 fi
-cd /TheMovieProviderBot
+cd /TheMovieProviderBot1
 pip3 install -U -r requirements.txt
 echo "Starting TheMovieProviderBot...."
 python3 bot.py
